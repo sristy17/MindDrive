cc4323106c9f43e57ce5753b1f4f5cfb9344ae79
Pooranjoy <pooranjoyb2016@gmail.com>
2024-07-04 23:50:36 +0530

    setup views

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
-import express from 'express'
-import bp from'body-parser'
-import authRoutes from "./routes/auth.routes.js"
-import geminiRoutes from "./routes/gemini.routes.js"
+import express from 'express';
+import bp from 'body-parser';
+import authRoutes from './routes/auth.routes.js';
+import geminiRoutes from './routes/gemini.routes.js';
 import connectDatabase from './database/Mongo.database.js';
 
 const app = express();
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-//setting up viewport for ejs
+// Setting up viewport for EJS
 app.set('view engine', 'ejs');
 
-app.use(express.static("./public"))
+app.use(express.static('public'));
 
 app.use(bp.json());
-app.use(bp.urlencoded({extended: false}))
+app.use(bp.urlencoded({ extended: false }));
 
-// BE routes 
+// BE routes
 app.use('/auth', authRoutes);
 app.use('/gemini', geminiRoutes);
 
-
 // Views
 app.get('/', (req, res) => {
-    res.render("../views/pages/index.ejs");
-})
+    res.render('pages/index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('pages/about');
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
     connectDatabase();
-})
+});
