0f94e67cbff58747e471f7fe05ac47179749790d
Swayam-hue <swayamg2005@gmail.com>
2024-10-01 23:12:37 +0530

    Added a scroll up button to the desktop page

--- public/js/global.js
+++ public/js/global.js
@@ -60,3 +60,23 @@ closebtn.addEventListener('click', () => {
     modal.classList.remove('show');
     modal.classList.add('hidden');
 });
+
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if(pos > 100){
+        scrollProgress.style.display = "grid";
+    }
+    else{
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#7C3AED ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+}
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
