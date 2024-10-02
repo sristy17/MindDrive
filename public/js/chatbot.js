e064d4ad5b6a21f04d74fe932485a01f560cd15a
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2024-10-02 16:46:46 +0530

    added

--- public/js/chatbot.js
+++ public/js/chatbot.js
@@ -136,3 +136,19 @@ sendBtn.addEventListener('click', () => {
         botmessage(user.message);
     }
 });
+
+
+const body = document.getElementById('body');
+const button = document.getElementById('theme-toggle');
+
+button.addEventListener('click', () => {
+  if (body.classList.contains('light-theme')) {
+    body.classList.remove('light-theme');
+    body.classList.add('dark-theme');
+    button.innerHTML = '<i class="fas fa-sun"></i>'; // Change icon for dark theme
+  } else {
+    body.classList.remove('dark-theme');
+    body.classList.add('light-theme');
+    button.innerHTML = '<i class="fas fa-moon"></i>'; // Change icon for light theme
+  }
+});
