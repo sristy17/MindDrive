061031eb7a0a471e4e1717ceb16df56cfd4b1c93
Animesh <animeshraj958@gmail.com>
2024-12-21 19:25:26 +0530

    code cleanup and bug checkup

--- public/js/Profile.js
+++ public/js/Profile.js
@@ -5,10 +5,10 @@ button.addEventListener('click', () => {
   if (body.classList.contains('light-theme')) {
     body.classList.remove('light-theme');
     body.classList.add('dark-theme');
-    button.innerHTML = '<i class="fas fa-sun"></i>'; // Change icon for dark theme
+    button.innerHTML = '<i class="fas fa-sun"></i>';
   } else {
     body.classList.remove('dark-theme');
     body.classList.add('light-theme');
-    button.innerHTML = '<i class="fas fa-moon"></i>'; // Change icon for light theme
+    button.innerHTML = '<i class="fas fa-moon"></i>'; 
   }
 });
