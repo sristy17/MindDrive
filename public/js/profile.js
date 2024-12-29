9b30c53858e98af21300d432b52318c96f8b7ed6
Animesh <animeshraj958@gmail.com>
2024-12-29 19:20:40 +0530

    dark mode on profile page added

---
+++ public/js/profile.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.getElementById('profilepage');
+  const button = document.getElementById('theme-toggle');
+  
+  button.addEventListener('click', () => {
+    if (body.classList.contains('light-theme')) {
+      body.classList.remove('light-theme');
+      body.classList.add('dark-theme');
+      button.innerHTML = '<i class="fas fa-sun"></i>';
+    } else {
+      body.classList.remove('dark-theme');
+      body.classList.add('light-theme');
+      button.innerHTML = '<i class="fas fa-moon"></i>';
+    }
+  });
+});
