1253572e22aae03937581f5f3feb024c5cd81152
Aritra Dey <adey01027@gmail.com>
2025-02-02 19:42:27 +0530

    refactor: removed unnecessary code in db file

--- database/Mongo.database.js
+++ database/Mongo.database.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
-// import { MONGODB_URI } from "../config/db.conf.js";
-const MONGODB_URI = process.env.MONGODB_URI;
+import { MONGODB_URI } from "../config/db.conf.js";
 const connectDatabase = () => {
     mongoose.set('strictQuery', false);
     mongoose.connect(MONGODB_URI);
